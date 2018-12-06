a5bedb1c9fb043fa30a8398d81978fae065dcaa1
Coach Caryn <cmccarthy15@gmail.com>
2018-12-06 12:49:00 -0500

    Adding app folder

---
+++ app/src/main/java/com/teamedge/android/droidgram/MainActivity.java
@@ -0,0 +1,21 @@
+package com.teamedge.android.droidgram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void decreaseLikes() {
+
+    }
+
+    public void increaseLikes() {
+
+    }
+}
